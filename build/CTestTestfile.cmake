2a453375fa00eae8a05c93436509b0cf4f3f73f6
intricos777-dot <intricos777@gmail.com>
2026-08-08 04:09:09 -0400

    chore: ignore build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sin/Projects/loz-majoras-mask
-# Build directory: /home/sin/Projects/loz-majoras-mask/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test("mm-core" "/home/sin/Projects/loz-majoras-mask/build/mm")
-set_tests_properties("mm-core" PROPERTIES  _BACKTRACE_TRIPLES "/home/sin/Projects/loz-majoras-mask/CMakeLists.txt;48;add_test;/home/sin/Projects/loz-majoras-mask/CMakeLists.txt;0;")
